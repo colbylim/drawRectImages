7bdbb5d492f4f7b458b339a3b16f3f81ab9dc438
colbylim <colbylim84@gmail.com>
2020-11-17 14:15:05 +0900

    BASE FILES

---
+++ drawRectImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  drawRectImages
+//
+//  Created by colbylim on 2020/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
