6d08fc8976195e62a0e042b6bd3db479d85fdc94
colbylim <colbylim84@gmail.com>
2020-11-18 13:58:05 +0900

    화면에 보이는 이미지만 로드되도록 구현

--- drawRectImages/MyView.swift
+++ drawRectImages/MyView.swift
@@ -8,34 +8,53 @@
 import UIKit
 
 class MyView: UIView {
+    
+    private static let borderColor = UIColor.random
+    
+    var isVisible: Bool = false
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//    }
+    
+//    VC에서 touch event 처리 하기 위해 현재 view 무시
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let hitView: UIView? = super.hitTest(point, with: event)
+        if (self == hitView) { return nil }
+        return hitView
+        
+    }
 
     override func draw(_ rect: CGRect) {
+//        화면에 보이지 않고 있다면 return
+        if isVisible == false {
+            return
+        }
+        
         guard let ctx = UIGraphicsGetCurrentContext() else {
             return
         }
         
+        ctx.saveGState()
+
 //        2. 커스텀 뷰에서 drawRect: 메시지를 구현하여 배경 화면을 임의의 색깔로 칠하게 하시오.
-        backgroundColor = UIColor.random
+        backgroundColor = .systemBackground
         backgroundColor?.setFill()
         ctx.fill(rect)
         
-//        4-1. 3번의 drawRect:에서 그린 이미지를 화면의 1/4 정도 크기의 섬네일로 표시하고, 윤곽선의 꼭지점을 둥글게 처리하고, 주어진 파일 이외의 이미지를 사용하면 안 됨.
-        let height = (rect.height / 4).rounded()
-        let imgRect = CGRect(x: rect.origin.x,
-                             y: (rect.height / 2) - (height / 2),
-                             width: rect.width,
-                             height: height)
-        
-        let path = UIBezierPath(roundedRect: imgRect, cornerRadius: 10)
+//        4-1. 윤곽선의 꼭지점을 둥글게 처리
+        let path = UIBezierPath(roundedRect: rect, cornerRadius: 10)
         ctx.addPath(path.cgPath)
         ctx.clip()
-                
+
 //        3. 2번의 drawRect: 구현만 변경하여 주어진 PNG 파일 중 하나를 로드하여 화면에 표시하시오.
-        UIImage(named: "image01")?.draw(in: imgRect)
-        
+        let imageName = String(format: "image%02d", tag)
+        UIImage(named: imageName)?.draw(in: rect)
+
 //        4-2. 그 주변을 임의의 색깔을 가진 윤곽선으로 둘러싸시오.
-        UIColor.random.setStroke()
+        MyView.borderColor.setStroke()
         path.lineWidth = 10
         path.stroke()
+        
+        ctx.restoreGState()
     }
 }
--- drawRectImages/ViewController.swift
+++ drawRectImages/ViewController.swift
@@ -9,12 +9,89 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var stackViewTopLayoutConstraint: NSLayoutConstraint!
+
+    let imageCount: Int = 60
+    lazy var maxPage: Int = {
+        return Int((CGFloat(imageCount) / 4).rounded(.up) - 1)
+    }()
+    
+    var views: [MyView] = [MyView]()
+    var page: Int = 0
+    var beganPoint: CGFloat = 0
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+
+        DispatchQueue.main.async { [weak self] in
+            self?.setupViews()
+        }
     }
-
-
+    
+    func setupViews() {
+        for i in 1 ... imageCount {
+            let v = MyView()
+            v.translatesAutoresizingMaskIntoConstraints = false
+            v.tag = i
+            
+            let h = view.safeAreaLayoutGuide.layoutFrame.size.height / 4
+            
+            views.append(v)
+            stackView.addArrangedSubview(v)
+            
+            v.addConstraint(NSLayoutConstraint(item: v,
+                                               attribute: .height,
+                                               relatedBy: .equal,
+                                               toItem: nil,
+                                               attribute: .height,
+                                               multiplier: 1,
+                                               constant: h))
+        }
+        
+        setMyViews(page: page, isVisible: true)
+    }
+    
+    func setMyViews(page: Int, isVisible: Bool) {
+        let startIndex = page * 4
+        let endIndex = page != maxPage ? 4 : imageCount % 4
+        views[startIndex ..< startIndex + endIndex].forEach({
+            $0.isVisible = isVisible
+            $0.setNeedsDisplay()
+        })
+    }
+        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first as UITouch? {
+            beganPoint = floor(touch.location(in: view).y)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first as UITouch? {
+            let endPoint = floor(touch.location(in: view).y)
+            let oldPage = page
+            if beganPoint > endPoint {
+                // down
+                if page == maxPage { return }
+                page += 1
+            } else {
+                // up
+                if page == 0 { return }
+                page -= 1
+            }
+                        
+            setMyViews(page: page, isVisible: true)
+            
+            let constant = -(CGFloat(page) * view.safeAreaLayoutGuide.layoutFrame.size.height)
+            stackViewTopLayoutConstraint.constant = constant
+            
+            UIView.animate(withDuration: 0.3) { [weak self] in
+                self?.view.layoutIfNeeded()
+            } completion: { [weak self] (_) in
+                self?.setMyViews(page: oldPage, isVisible: false)
+            }
+        }
+    }
 }
 
